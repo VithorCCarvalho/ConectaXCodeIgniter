472267c25f585327198c07ec41ea1eb1b40bd8ca
VithorCCarvalho <j.vithor13@gmail.com>
2018-12-09 01:34:28 -0200

    1.1

--- application/config/database.php
+++ application/config/database.php
@@ -4,10 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if(file_exists('database.inc.php'))
-{
-	require 'database.inc.php';
-}
+include_once 'database.inc.php';
 
 $db['default'] = array(
 	'dsn'	=> '',
